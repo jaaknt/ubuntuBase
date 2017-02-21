44075624ebc406517764160aaebfadcb411d9b83
Jaak Niit <jaak.niit@gmail.com>
2017-02-21 22:01:26 +0000

    ANXS.postgres configuration

--- ansible/files/scripts/install_docker_machine_ubuntu16.04.sh
+++ ansible/files/scripts/install_docker_machine_ubuntu16.04.sh
@@ -4,5 +4,5 @@
 
 DOCKER_MACHINE_VERSION=0.9.0
 
-sudo curl -L "https://github.com/docker/machine/releases/download/v${DOCKER_MACHINE_VERSION}/docker-machine-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-machine && \
+sudo curl -L "https://github.com/docker/machine/releases/download/v${DOCKER_MACHINE_VERSION}/docker-machine-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-machine && \
 sudo chmod +x /usr/local/bin/docker-machine
