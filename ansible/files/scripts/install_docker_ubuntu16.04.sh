b617ee4776f3974f5645f36074ec62218b8687fd
Jaak Niit <jaak.niit@gmail.com>
2017-03-14 18:40:45 +0200

    docker-ce installation script upgrade

--- ansible/files/scripts/install_docker_ubuntu16.04.sh
+++ ansible/files/scripts/install_docker_ubuntu16.04.sh
@@ -5,8 +5,7 @@ sudo apt-get -y --no-install-recommends install curl apt-transport-https ca-cert
 
 curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
 
-sudo add-apt-repository "deb https://apt.dockerproject.org/repo/ ubuntu-$(lsb_release -cs) main"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 sudo apt-get update
-sudo apt-get -y install docker-engine
-
+sudo apt-get -y install docker-ce
